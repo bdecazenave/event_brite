5710f12b94c7fc14572ba250f3cb8ab27c476afd
bdecazenave <benjamindecazenave@gmail.com>
2021-11-08 20:11:39 +0100

    coucouu boris

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,12 @@
 class Attendance < ApplicationRecord
+  after_create :event_confirmation_send
   belongs_to :user
   belongs_to :event
+
+
+  def event_confirmation_send
+     AttendanceMailer.event_confirmation_email(self).deliver_now
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 # Un User peut avoir plusieurs réservations et il peut avoir plusieurs events au travers des réservations
 class User < ApplicationRecord
+  after_create :welcome_send
   has_many :attendances
   has_many :events, through: :attendances
   has_many :created_event, foreign_key:'admin_id', class_name: 'User'
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
 end
